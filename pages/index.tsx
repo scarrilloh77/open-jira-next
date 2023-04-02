6f1695ad80b77481aac161347ff69942773b4a5a
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-02 09:18:20 -0500

    initial template for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
-import { Typography } from '@mui/material';
+import { Card, CardHeader, Grid, Typography } from '@mui/material';
 import { NextPage } from 'next';
 import React from 'react';
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout title="Open Jira">
-      <Typography variant="h1" color="primary">
-        HomePage
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Pendientes" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="En progreso" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
