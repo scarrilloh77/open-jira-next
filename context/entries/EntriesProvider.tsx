01bb861e82d39c60b45111293ab99f9b4a28f373
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-13 22:24:31 -0500

    actualizar entry desde el front

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -19,8 +19,16 @@ export const EntriesProvider: FC = ({ children }) => {
     dispatch({ type: '[Entry] - Add-Entry', payload: data });
   };
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: '[Entry] - Entry-Updated', payload: entry });
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      }); // OJO CON EL CONSUMO.
+      dispatch({ type: '[Entry] - Entry-Updated', payload: data });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const refreshEntries = async () => {
